0dae1c88d41c14c32742d7b3d8096a60c31057c0
Jo Tadano <jotadano@gmail.com>
2021-12-12 20:05:37 +0900

    jsでボタンを作りました

--- quizy/index.js
+++ quizy/index.js
@@ -1,13 +1,36 @@
 'use strict'
 
 const true1 = document.getElementById('true1');
+const false1 = document.getElementById('false1');
+const false2 = document.getElementById('false2');
 
 const truebox1 = document.getElementById('truebox1');
+const falsebox1 = document.getElementById('falsebox1');
 
 true1.onclick = () => {
-  true1.style.backgroundColor = "blue";
+  true1.style.backgroundColor = "#287dff";
   true1.style.color = "white";
   truebox1.style.display = "block";
+  true1.disabled = true;
+  false1.disabled = true;
+  false2.disabled = true;
 };
 
-console.log(true1);
+false1.onclick = () => {
+  false1.style.backgroundColor = "#ff5128";
+  false1.style.color = "white";
+  falsebox1.style.display = "block";
+  true1.style.backgroundColor = "#287dff";
+  true1.style.color = "white";
+};
+
+false2.onclick = () => {
+  false2.style.backgroundColor = "#ff5128";
+  false2.style.color = "white";
+  falsebox1.style.display = "block";
+  true1.style.backgroundColor = "#287dff";
+  true1.style.color = "white";
+};
+
+// document.getElementById("true1").disabled = true;
+// console.log(true1);
