3d8ebd32c825bbc063bee2d0e62cfb1a5a059be1
Jo Tadano <jotadano@gmail.com>
2022-02-11 17:21:55 +0900

    Quizyシャッフルを実装しました

--- quizy/index.js
+++ quizy/index.js
@@ -41,17 +41,17 @@
 // document.getElementById("true1").disabled = true;
 // console.log(true1);
 
-let number = ['1','2','3']
+// let number = ['1','2','3']
 let picture = ['高輪','亀戸','麹町','御成門','等々力','石神井','雑色','御徒町','鹿骨','小榑'] //写真
 let choice = [['たかなわ','たかわ','こうわ'],['かめいど','かめど','かめと'],['こうじまち','かゆまち','おかとまち'],['おなりもん','おかどもん','ごせいもん'],['とどろき','たたりき','たたら'],['しゃくじい','いじい','せきこうい'],['ぞうしき','ざっしき','ざっしょく'],['おかちまち','ごしろちょう','みとちょう'],['ししぼね','ろっこつ','しこね'],['こぐれ','こしゃく','こばく']]; //選択肢
 
-// function shuffle(arr) {
-//   for (let k = arr.length - 1; k > 0; k--) {
-//     const j = Math.floor(Math.random() * (k + 1));
-//     [arr[j], arr[k]] = [arr[k], arr[j]];
-//   }
-//   return arr;
-// }
+function shuffle(arr) {
+  for (let k = arr.length - 1; k > 0; k--) {
+    const j = Math.floor(Math.random() * (k + 1));
+    [arr[j], arr[k]] = [arr[k], arr[j]];
+  }
+  return arr;
+}
 
 
 
@@ -71,13 +71,14 @@ for(let i = 0; i<10; i++){
   let choices = ['<li class="choice" id="true1'+[i]+'">'+choice[i][0]+'</li>'
   ,'<li class="choice" id="false1'+[i]+1+'">'+choice[i][1]+'</li>'
   ,'<li class="choice" id="false2'+[i]+2+'">'+choice[i][2]+'</li>']
-  shuffle(choices);
+  const shuffled = shuffle(choices);
+  
   main += '<h2 class="title">'+[i+1]+'.この地名はなんて読む？</h2>'+'<img src="img/'+picture[i]+'.png" alt="地名">'
 
   +'<ul class="choices" id="choices">'
-  +choices[0]
-  +choices[1]
-  +choices[2]
+  +shuffled[0]
+  +shuffled[1]
+  +shuffled[2]
   +'</ul>'
 
   +'<div class="resultbox" class="truebox" id="truebox'+[i]+'">'
@@ -85,7 +86,7 @@ for(let i = 0; i<10; i++){
   +'<p class="resultbox_answer">正解は「'+choice[i][0]+'」です！</p>'
   +'</div>'
 
-  +'<div class="resultbox" class="falsebox" id="falsebox">'
+  +'<div class="resultbox" class="falsebox" id="falsebox'+[i]+'">'
   +'<p class="falsebox_text">不正解！</p>'
   +'<p class="resultbox_answer">正解は「'+choice[i][0]+'」です！</p>'
   +'</div>';
